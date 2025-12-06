5542b53aef08b2cddd5dc1e993bbad311382b3ae
hasnaaitbenbrahim <hasnaaitbenbrahim20@gmail.com>
2025-12-06 20:25:38 +0100

    first commit

---
+++ client-service/src/main/java/com/example/client/entities/Client.java
@@ -0,0 +1,37 @@
+package com.example.client.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Client() {
+    }
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ eureka-server/src/main/java/com/example/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer  // Active les fonctionnalités du serveur Eureka
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
